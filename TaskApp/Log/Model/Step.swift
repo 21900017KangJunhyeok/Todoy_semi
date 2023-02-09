bd308dd33bd5b78758a970f64bb48ef747e9ee38
JH_Kang <21900017@handong.ac.kr>
2023-02-09 15:43:29 +0900

    log sample code ( no core data )

---
+++ TaskApp/Log/Model/Step.swift
@@ -0,0 +1,22 @@
+//
+//  Step.swift
+//  TaskApp
+//
+//  Created by junhyeok KANG on 2023/02/07.
+//
+
+import SwiftUI
+
+struct Step: Identifiable{
+    var id = UUID().uuidString
+    var value: CGFloat
+    var key: String
+    var color: Color = Color(.purple)
+}
+
+var step: [Step] = [
+    Step(value: 500, key: "1-4 AM"),
+    Step(value: 240, key: "5-8 AM", color:  Color(.green)),
+    Step(value: 350, key: "9-11 AM"),
+    Step(value: 500, key: "1-4 PM"),
+]
